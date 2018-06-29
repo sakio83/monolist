924448766c3b71911842b6c29790608735b61529
saki.ota <saki.a.ota@rakuten.com>
2018-06-29 06:29:50 +0000

    have

---
+++ database/migrations/2018_06_29_125616_create_item_have_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemHaveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_have', function (Blueprint $table) {
+           $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('item_id')->unsigned()->index();
+            $table->string('type')->index();
+            $table->timestamps();
+            
+             // Foreign key settings
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_have');
+    }
+}
